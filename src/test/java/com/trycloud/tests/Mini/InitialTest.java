9878ef746f1939b5a2066bc0af3c341f319dc93a
minicandoit <74155239+minicandoit@users.noreply.github.com>
2021-01-31 19:51:38 -0500

    mini's second commit

--- src/test/java/com/trycloud/tests/Mini/InitialTest.java
+++ src/test/java/com/trycloud/tests/Mini/InitialTest.java
@@ -3,5 +3,6 @@ package com.trycloud.tests.Mini;
 public class InitialTest {
     public static void main(String[] args) {
         System.out.println("first commit");
+        //another commit
     }
 }
