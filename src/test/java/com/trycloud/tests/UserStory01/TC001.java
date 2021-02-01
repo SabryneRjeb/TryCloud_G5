fdde9b38cb9cc0ebb42baed2b6e8b5f68fa160b9
hoshangwardak <hoshangwardak@gmail.com>
2021-01-31 20:54:40 -0500

    UserStory01-->TC001

---
+++ src/test/java/com/trycloud/tests/UserStory01/TC001.java
@@ -0,0 +1,26 @@
+package com.trycloud.tests.UserStory01;
+
+import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC001 extends TestBase {
+
+    @Test
+    public void VerificationOfURL() {
+
+//        5. Verify the URL is changed to homepage’s url
+//        (Put this test case in testBase class with an annotation so it runs for all the test case)
+
+        //System.out.println(driver.getCurrentUrl());
+
+ Assert.assertTrue(driver.getCurrentUrl().equals("https://app.trycloud.net/index.php/apps/files/?dir=/&fileid=22622"),
+                "Failed, The URL is not the same");
+
+    }
+
+
+
+
+}
--- src/test/java/com/trycloud/tests/base/TestBase.java
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -1,7 +1,10 @@
 package com.trycloud.tests.base;
 
+import com.google.common.base.Verify;
 import com.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,14 +13,40 @@ import java.util.concurrent.TimeUnit;
 public abstract class TestBase {
     public WebDriver driver;
     @BeforeMethod
-    public void setUpMethod(){
+    public void setUpMethod() throws InterruptedException {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+//        1. Story: As a user, I should be able to login.
+//                Test case #1 - verify user login successfully
+//        1. Navigate to login page http://qa.trycloud.net/index.php/login?clear=1
+        driver.get("https://trycloud.net/");
+
+        WebElement loginLink = driver.findElement(By.xpath("(//a[.='Login'])[1]"));
+        Thread.sleep(2000);
+        loginLink.click();
+
+//        2. Enter valid username
+        WebElement usernameInputBox = driver.findElement(By.xpath("//input[@name='user']"));
+        Thread.sleep(1000);
+        usernameInputBox.sendKeys("User5");
+
+//        3. Enter valid password
+        WebElement passwordInputBox = driver.findElement(By.xpath("//input[@name='password']"));
+        Thread.sleep(1000);
+        passwordInputBox.sendKeys("Userpass123");
+
+//        4. Click login button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@id='submit']"));
+        loginButton.click();
+        Thread.sleep(2000);
+
     }
-    @AfterMethod
-    public void tearDown(){
-        driver.close();
-    }
+
+    //@AfterMethod
+  //  public void tearDown(){
+       // driver.close();
+  //  }
 
 }
