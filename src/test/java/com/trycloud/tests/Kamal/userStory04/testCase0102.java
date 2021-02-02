baaf3fc5fd381fd7e65a56f9ec63e93300bba886
kamalkablan <kimokablan10@gmail.com>
2021-02-02 11:35:38 -0500

    User Story 4 test case 01 test case 02 are done

---
+++ src/test/java/com/trycloud/tests/Kamal/userStory04/testCase0102.java
@@ -0,0 +1,39 @@
+package com.trycloud.tests.Kamal.userStory04;
+
+import com.trycloud.tests.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class testCase0102 extends TestBase {
+    @Test
+
+    public void talkModules (){
+        //Test case #1 - verify users can access to Talks module
+        //1.Login as a user2.
+        // Click Talks module3.Verify the page tile is Talks module’s tile
+        WebElement talks = driver.findElement(By.xpath("//a[@aria-label='Talk']"));
+        talks.click();
+
+            //1.Login as a user
+            // 2.Click Talks module
+            // 3.Search a user from search box on the left
+            // 4.Write a message
+            // 5.Click submit button
+            // 6.Verify the message is displayed on the conversation log
+
+
+        WebElement chat = driver.findElement(By.xpath("//div[@data-user='Employee1']"));
+        chat.click();
+        WebElement message = driver.findElement(By.xpath("//div[@class='message']"));
+        message.sendKeys("Hello How Are You");
+        WebElement send = driver.findElement(By.xpath("//input[@class='submit icon-confirm has-tooltip']"));
+        send.click();
+
+
+    }
+
+    }
+
+
