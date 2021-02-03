12345166046ccf48990e088fe038c45a578a8feb
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-02-03 13:11:06 -0500

    commiting afterMethod

---
+++ src/test/java/com/trycloud/tests/Sabryne/UserStory5_TestCase001.java
@@ -0,0 +1,30 @@
+package com.trycloud.tests.Sabryne;
+
+import com.trycloud.tests.base.TestBase;
+import org.testng.annotations.Test;
+
+public class UserStory5_TestCase001 extends TestBase {
+//Test case #3 - verify users can see all the contact names on the contact list
+//1. Login as a user
+//2. Click contacts module
+//3. Verify the contact names are in the list
+//(Pre-condition: there should be at least 2 contact names are displayed On the contact list)
+
+
+    @Test
+
+    public void allNamesOnTheLIst(){
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
