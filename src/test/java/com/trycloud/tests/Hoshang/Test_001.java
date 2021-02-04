1019556b3ffd6890acee9d0868572ef887627389
hoshangwardak <hoshangwardak@gmail.com>
2021-02-03 21:08:09 -0500

    Initial commit

--- src/test/java/com/trycloud/tests/Hoshang/Test_001.java
+++ src/test/java/com/trycloud/tests/Hoshang/Test_001.java
@@ -3,6 +3,6 @@ package com.trycloud.tests.Hoshang;
 public class Test_001 {
     public static void main(String[] args) {
 
-        System.out.println("Hell Team");
+        System.out.println("Hello Team, great to see you guys");
     }
 }
